65c0949ff918879a4cfbfeafc0e404b23c4f966c
이진수 <leejinsoo0412@gmail.com>
2022-12-02 17:14:56 +0900

    flask 업데이트

--- model/detector/binary_esg_flask.py
+++ model/detector/binary_esg_flask.py
@@ -5,22 +5,14 @@ CONFIG_FILE = 'configs/focalnet/focalnet_binary_tiny_sparse_rcnn.py'
 CHECKPOINT_PATH = 'data/pretrain/focal_sparse_rcnn_epoch_17.pth'
 
 model = init_detector(CONFIG_FILE, CHECKPOINT_PATH, device='cpu')  # or device='cuda:0'
-imgs = ['data/binary/cctv_esg/test_001.jpg','data/binary/cctv_esg/test_002.jpg','data/binary/cctv_esg/test_003.jpg']
-results = inference_detector(model, imgs)
+
 
 binary_esg_flask = Flask(__name__)
 @binary_esg_flask.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files['file']
-        img_bytes = file.read()
-        #img_bytes = request.input_stream.read()
-        print("check\n")
-        class_id, class_name = get_prediction(image_bytes=img_bytes)
-        print(class_id, class_name)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
-        #return "hello"
-
-
+        imgs = ['data/binary/cctv_esg/test_001.jpg','data/binary/cctv_esg/test_002.jpg','data/binary/cctv_esg/test_003.jpg']
+        results = inference_detector(model, imgs)
+        return results
 if __name__ == '__main__':
     binary_esg_flask.run()
